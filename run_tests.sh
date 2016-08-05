2a0a4146fc68aba8b935596dea4f61d0a24849d9
Romain Acciari <romain.acciari@gmail.com>
2016-08-05 13:14:58 +0200

    Update oio-reset.sh

--- run_tests.sh
+++ run_tests.sh
@@ -62,7 +62,8 @@ sudo pip install nose-htmloutput
 function run_single_instance () {
   cd ${TMPDIR}/oio-sds
   export NOSE_ARGS="--with-xunit --xunit-file=${TMPDIR}/nosestests-single.xml --with-html --html-file=${TMPDIR}/nosestests-single.html"
-  tools/oio-reset.sh -S SINGLE -X sqlx -X zookeeper -R 1 -B 1
+#  tools/oio-reset.sh -S SINGLE -X sqlx -X zookeeper -R 1 -B 1
+  tools/oio-reset.sh -N NS -f etc/bootstrap-SINGLE.yml
   tox -e func
 }
 
